d23904c8295faaeb795a0c9e5a82d9e3b1f4d6f4
coder0104 <binbin080104@gmail.com>
2025-02-03 23:09:41 +0900

    그리디

---
+++ 그리디.py
@@ -0,0 +1,12 @@
+#거스름돈 문제(1260원을 500, 100, 50, 10원으로 거스를 수 있는 동전의 최소 개수)
+n = 1260
+count = 0
+
+coin_array = [500, 100, 50, 10]
+
+for coin in coin_array:
+  count += n // coin
+  n %= coin
+
+print(count)
+#시간복잡도 O(k)
